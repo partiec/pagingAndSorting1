7d0dc886e2b499c84a6b475d5cecd13bebcf321c
Alexey Frolov <frolov.git@mail.ru>
2024-07-27 16:00:33 +0300

    get(), getWithSort(), getWithPagination(), getWithPaginationAndSort()

--- src/main/java/ru/frolov/pagingandsorting/controller/ProductController.java
+++ src/main/java/ru/frolov/pagingandsorting/controller/ProductController.java
@@ -1,7 +1,9 @@
 package ru.frolov.pagingandsorting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.frolov.pagingandsorting.dto.APIResponse;
@@ -21,5 +23,30 @@ public class ProductController {
     private APIResponse<List<Product>> getProducts() {
         List<Product> allProducts = this.service.findAllProducts();
         return new APIResponse<>(allProducts.size(), allProducts);
+    }
+
+    @GetMapping("{field}")
+    private APIResponse<List<Product>> getProductsWithSort(@PathVariable("field") String field) {
+        List<Product> allProducts = this.service.findProductsWithSorting(field);
+        return new APIResponse<>(allProducts.size(), allProducts);
+    }
+
+    @GetMapping("pagination/{offset}/{pageSize}")
+    private APIResponse<Page<Product>> getProductsWithPagination(
+            @PathVariable int offset,
+            @PathVariable int pageSize) {
+
+        Page<Product> productsWithPagination = this.service.findProductsWithPagination(offset, pageSize);
+        return new APIResponse<>(productsWithPagination.getSize(), productsWithPagination);
+    }
+
+    @GetMapping("paginationAndSort/{offset}/{pageSize}/{field}")
+    private APIResponse<Page<Product>> getProductsWithPaginationAndSort(
+            @PathVariable int offset,
+            @PathVariable int pageSize,
+            @PathVariable String field) {
+
+        Page<Product> productsWithPagination = this.service.findProductsWithPaginationAndSorting(offset,pageSize,field);
+        return new APIResponse<>(productsWithPagination.getSize(), productsWithPagination);
     }
 }
--- src/main/java/ru/frolov/pagingandsorting/service/ProductService.java
+++ src/main/java/ru/frolov/pagingandsorting/service/ProductService.java
@@ -1,16 +1,14 @@
 package ru.frolov.pagingandsorting.service;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.frolov.pagingandsorting.entity.Product;
 import ru.frolov.pagingandsorting.repository.ProductRepository;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Service
 @Data
@@ -18,15 +16,29 @@ public class ProductService {
 
     private final ProductRepository repository;
 
-    @PostConstruct
-    public void initDB() {
-        List<Product> products = IntStream.rangeClosed(1, 200)
-                .mapToObj(i -> new Product("product" + i, new Random().nextInt(100), new BigDecimal(new Random().nextDouble(100))))
-                .collect(Collectors.toList());
-        repository.saveAll(products);
-    }
+//    @PostConstruct
+//    public void initDB() {
+//        List<Product> products = IntStream.rangeClosed(1, 200)
+//                .mapToObj(i -> new Product("product" + i, new Random().nextInt(100), new BigDecimal(new Random().nextDouble(100))))
+//                .collect(Collectors.toList());
+//        repository.saveAll(products);
+//    }
 
     public List<Product> findAllProducts() {
         return this.repository.findAll();
+    }
+
+    public List<Product> findProductsWithSorting(String field) {
+        return repository.findAll(Sort.by(Sort.Direction.ASC, field));
+    }
+
+    public Page<Product> findProductsWithPagination(int offset, int pageSize) {
+        Page<Product> products = this.repository.findAll(PageRequest.of(offset, pageSize));
+        return products;
+    }
+
+    public Page<Product> findProductsWithPaginationAndSorting(int offset, int pageSize, String field) {
+        Page<Product> products = this.repository.findAll(PageRequest.of(offset, pageSize).withSort(Sort.by(field)));
+        return products;
     }
 }
